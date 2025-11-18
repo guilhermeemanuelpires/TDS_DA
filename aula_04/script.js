8e62d02f91e3bebca1f4869533c5fb5c33ea7302
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-11-18 20:47:46 -0300

    teste

--- aula_04/script.js
+++ aula_04/script.js
@@ -1,7 +1,6 @@
-function add(){
+function add() {
     //Carregando o elemento ul que tem o ID "itens"
     var lista = document.getElementById('itens');
-
     //Carregando o elemento input que tem o id "campo"
     var campo = document.getElementById('campo');
 
@@ -12,20 +11,18 @@ function add(){
     // para nosso elemento li
     li.innerHTML = campo.value;
 
-    
     var btn = document.createElement('button');
-    btn.innerHTML = "Apagar";
-    btn.addEventListener('onclcik', apagarElemento());
-     
+    btn.innerHTML = "Excluir";
+    btn.addEventListener('click', function () {
+        lista.removeChild(this.parentNode)
+    });
+
     li.appendChild(btn);
+
     //Aplicamos o elemento li dentro do elemento ul
     lista.appendChild(li);
 
     //Limpamos a propriedade value do nosso elemento campo 
     campo.value = "";
 
-}
-
-function apagarElemento(elemento){
-    document.removeChild(elemento);    
 }
