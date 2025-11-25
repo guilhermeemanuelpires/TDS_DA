7056277c0cc54f5e1805591732714e233b179781
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-11-24 21:44:52 -0300

    aula 06

--- aula_06/script.js
+++ aula_06/script.js
@@ -1,12 +1,44 @@
-var dado = localStorage.getItem('dado');
-var input = document.getElementById("campo");
+const dados = localStorage.getItem("dados");
+const input = document.getElementById("campo");
 
-input.value = dado;
+// input.value = dados;
 
 function salvar() {
+    pessoas.push(input.value)
 
-    var nome = input.value;
+    const h1 = document.createElement("h1");
 
-    localStorage.setItem("dado", nome);
+    h1.innerHTML = input.value;
 
+    document.body.appendChild(h1);
+    SalvaNaMemoriaDoNavegador()
 }
+
+function excluir() {
+    localStorage.removeItem("dados");
+    input.value = "";
+}
+
+
+function SalvaNaMemoriaDoNavegador(){
+    localStorage.setItem("dados", JSON.stringify(pessoas));
+}
+
+const pessoas = JSON.parse(dados);
+
+Mostra();
+
+
+function Mostra() {
+
+    console.log(pessoas);
+
+    for (var i = 0; i < pessoas.length; i++) {
+        const h1 = document.createElement("h1");
+
+        h1.innerHTML = pessoas[i];
+
+        document.body.appendChild(h1);
+    }
+}
+
